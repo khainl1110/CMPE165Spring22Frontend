d5303cb33602a1c7f990ee126b5c66fc7ac1908a
Khai Nguyen <khainl1110@gmail.com>
2022-02-26 17:26:11 -0800

    Demo database

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -19,6 +19,7 @@ export default function App() {
 
             <Link to="/login">Log In</Link> |
             <Link to = "/signup" >Sign Up</Link>
+            <Link to = '/hotelTest'>Hotel Test </Link>
         </>
     )
 }
---
+++ src/Components/HotelPage/HotelPage.jsx
@@ -0,0 +1,38 @@
+import {useEffect, useState} from 'react';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+
+export default function HotelPage() {
+
+    let [hotels, setHotels] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:8080/hotels/testGet')
+        .then(data => data.json())
+        .then(data => {
+            console.log(data)
+            setHotels(data)
+        })
+    })
+
+    return (
+        <div>
+            <Typography variant="h2" component="div" gutterBottom>
+                This is list of hotels
+            </Typography>
+            <Grid container>
+            {
+                hotels.map( hotel => {
+                    return(
+                        <Grid item>
+                        <h1>{hotel.hotelName}</h1>
+                        <h2>{hotel.hotelAddress} </h2>
+                        <img src = {hotel.picUrl} alt = "test" width = "300" height = "300"/>
+                        </Grid>
+                    ) 
+                })
+            }
+            </Grid>
+        </div>
+    )
+}
