0b974b4ca8ee835754f9aa1aa53ad9bc43277670
Aadit Dubey <dubeyaadit@gmail.com>
2022-03-05 20:20:39 -0800

    resolved the errors hopefully it pushes without problem

--- src/Components/LogInPage/LogInPage.jsx
+++ src/Components/LogInPage/LogInPage.jsx
@@ -1,11 +1,152 @@
-import Typography from '@mui/material/Typography';
+import { useState } from "react";
+import {
+  Button,
+  CssBaseline,
+  TextField,
+  FormControl,
+  Grid,
+  Box,
+  Typography,
+  Container
+} from '@mui/material/';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import styled from 'styled-components';
+
+import Image from '../../assets/image6.png';
+import { Paper } from '@mui/material';
+
+import syk from '../LogInPage/login.module.css';
 
 export default function LogInPage() {
-    return (
-        <div>
-            <Typography variant="h2" component="div" gutterBottom>
-                This is log in page
-            </Typography>
+
+  const theme = createTheme();
+  const Boxs = styled(Box)`padding-bottom: 0%;`;
+
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const styles = {
+    paperContainer: {
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      height: '100vh',
+      width: '100%',
+      backgroundImage: `url(${Image})`
+    }
+  };
+
+
+  const handleLogIn = (e) => {
+    console.log("abcd");
+  };
+
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Paper style={styles.paperContainer}>
+      <div className = {syk.header}>
+        <ul className = {syk.headerUl}>
+            <li>Like Home</li>
+        </ul>     
         </div>
-    )
+        <Container component="main" justifyContent="flex-start" >
+          <CssBaseline />
+          
+          <Grid container direction="row" justifyContent="flex-start" alignItems="center">
+            <Grid item xs={4}>
+              <Box
+                sx={{
+                  position: "absolute",
+                  marginTop: '15%',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  width: '40%',
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontFamily: 'Baloo Bhaina 2',
+                    fontStyle: 'normal',
+                    fontSize: '60px',
+                    letterSpacing: '0.005em',
+                    color: '#FFFFFF',
+                    width: '100%'
+                  }}
+                >
+                  Where to next?
+                </Typography>
+              </Box>
+            </Grid>
+
+            <Grid item xs={0}>
+              <Box
+                sx={{
+                  position: "absolute",
+                  marginTop: '8%',
+                  maxWidth: '40%',
+                  marginLeft: '15%',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  backgroundColor: 'rgba(239, 241, 237, 0.9)',
+                  borderRadius: '18px',
+                  padding: '30px',
+                }}
+              >
+                <Typography
+                  sx={{
+                    marginTop: '16px',
+                    fontSize: 24,
+                    fontWeight: 'bold',
+                    color: 'grey'
+                  }}>
+                    Login
+                </Typography>
+                <Boxs component="form" noValidate onSubmit={handleLogIn} sx={{ mt: 3 }}>
+                  <FormControl component="fieldset" variant="standard">
+                    <Grid container spacing={4}>
+                      
+                      <Grid item xs={12}>
+                        <TextField
+                          required
+                          autoFocus
+                          fullWidth
+                          type="email"
+                          id="email"
+                          name="email"
+                          label="Email"
+                          error={emailError !== '' || false}
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <TextField
+                          required
+                          fullWidth
+                          type="password"
+                          id="password"
+                          name="password"
+                          label="Password"
+                        />
+                      </Grid>
+                      
+
+                    </Grid>
+                    <Button onSubmit = {handleLogIn()}
+                      type="submit"
+                      variant="contained"
+                      sx={{ mt: 5, mb: 1, backgroundColor: '#9BB40D', fontWeight: '500' }}
+                    >
+                      Login
+                    </Button>
+                  </FormControl>
+                </Boxs>
+              </Box>
+            </Grid>
+          </Grid>
+        </Container>
+      </Paper>
+    </ThemeProvider>
+  )
 }
